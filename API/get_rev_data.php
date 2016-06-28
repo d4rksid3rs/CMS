81566becb196d0c71bf392f58a084eab83126947
d4rksid3rs <gaproghe@gmail.com>
2016-06-28 16:18:20 +0700

    Get Revenue Data

--- API/get_rev_data.php
+++ API/get_rev_data.php
@@ -18,20 +18,15 @@ require('db.class.php');
 //}
 
 $today = date('Y-m-d');
-//$today = date('2016-06-24');
+//$today = date('2016-06-28');
 $yesterday = date("Y-m-d", strtotime("-1 day", strtotime($today)));
 $yesterday_start = $yesterday. ' 00:00:00';
 $yesterday_end = $yesterday. ' 23:59:59';
-$sql = "select * from request "
-        . "where  created_on >= '{$yesterday_start}' AND created_on <= '{$yesterday_end}' and success > 0";
+$sql = "select type, sum(money) as sum_money from log_nap_koin "
+        . "where  created_on >= '{$yesterday_start}' AND created_on <= '{$yesterday_end}' group by type";
 $total = 0;
-foreach ($db->query($sql) as $row) {    
-    $total = $total + $row['cardvalue'];
+foreach ($db->query($sql) as $row) {       
+    $sql_insert = "INSERT INTO revenue (date_created, type, partner, k2, mv, total) VALUES ('{$yesterday}', '{$row['type']}', 'partner', '0', '0', '{$row['sum_money']}')";
+    $db->exec($sql_insert);
 }
-$sql_insert = "INSERT INTO revenue (date_created, type, partner, k2, mv, total) VALUES ('{$yesterday}', '2', 'partner', '0', '0', '{$total}')";
-$result = $db->exec($sql_insert);
-if ($result) {
-    echo "Get Revenue success !!!!!!!";
-} else {
-    echo "Get Revenue failed  ???????";
-}
+echo 'Success !!!!';
