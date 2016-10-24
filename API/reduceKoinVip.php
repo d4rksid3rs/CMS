ee36e8028f03f5b2f4bbcd47e9f8c3e2a68f111c
d4rksid3rs <gaproghe@gmail.com>
2016-10-24 10:03:41 +0700

    pwd reduce Koin vip

--- API/reduceKoinVip.php
+++ API/reduceKoinVip.php
@@ -14,7 +14,7 @@ $cause = mysql_escape_string($cause);
 if (is_numeric($koin) && strlen($pass) > 0 && strlen($user) > 0) {
     if ($koin >= 0) {
         $koin = $koin * (-1);
-        if ($pass == "admincongchip123") {
+        if ($pass == "pwdaddmoeny123$%^") {
             try {
                 $db->query("SET NAMES 'UTF8'");
                 $sql = "select * from auth_user where username='" . $user . "' limit 0,1";
